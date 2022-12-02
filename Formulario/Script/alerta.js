97ae6a0059d54852ad73c1f3a0863329839a5fd2
FabricaSoluciones12 <118380481+FabricaSoluciones12@users.noreply.github.com>
2022-12-02 14:54:04 -0500

    Add files via upload

---
+++ Formulario/Script/alerta.js
@@ -0,0 +1,6 @@
+Swal.fire(
+    'BIENVENIDO',
+  'Estimado estudiante, queremos acompañarte en el reto de terminar tu programa de formación en el tiempo planeado y de manera satisfactoria. #ConectateconbienestarUE',
+    'info',
+)
+
